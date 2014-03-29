1dbeda41d7591e5cef3b026718cd22feac7ca39d
Andrew George <ageorge@teknee.com>
2014-03-28 21:58:17 -0700

    Just a little house keeping

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,11 +3,4 @@ $(document).ready(function(){
 
     reveal = $('.reveal').data('tek.reveal');
 
-    $(document)
-        .on('show.bs.reveal', function () {
-            console.log('Show!');
-        })
-        .on('hide.bs.reveal', function () {
-            console.log('Hide!');
-        })
 });
